214909042d38a3f6ca068cc71e972c4c38b34b97
Suraj Vijay <surajcpp115@gmail.com>
2022-05-02 09:31:22 +0530

    ref to state in search field

--- src/components/search-field/index.tsx
+++ src/components/search-field/index.tsx
@@ -1,6 +1,6 @@
 import React, {
-  useRef, 
-  KeyboardEventHandler, MutableRefObject
+  useState, useRef, 
+  KeyboardEventHandler, MutableRefObject, ChangeEventHandler
 } from "react";
 
 import {
@@ -14,14 +14,17 @@ interface SearchFieldProps {
 }
 
 const SearchField: React.FC<SearchFieldProps> = ({cb, placeholder}) => {
+  const [text, setText] = useState<string>('');
   const textRef = useRef() as MutableRefObject<HTMLInputElement>;
 
-  const callCallBack = () => cb(textRef.current.value);
+  const callCallBack = () => cb(text);
   const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
     if (event.key !== "Enter") return;
     callCallBack();
   };
+  const handleTextChange: ChangeEventHandler<HTMLInputElement> = (event) => setText(event.target.value);
   const onClear = () => {
+    setText('');
     cb('');
     textRef.current.value = '';
     textRef.current.blur();
@@ -33,9 +36,10 @@ const SearchField: React.FC<SearchFieldProps> = ({cb, placeholder}) => {
       <SearchInput
         placeholder={placeholder}
         onKeyDown={onKeyDown}
+        onChange={handleTextChange}
         ref={textRef}
       />
-      {textRef.current?.value.length > 0 && <span onClick={onClear}>x</span>}
+      {text.length > 0 && <span onClick={onClear}>x</span>}
     </SearchContainer>
   );
 };
